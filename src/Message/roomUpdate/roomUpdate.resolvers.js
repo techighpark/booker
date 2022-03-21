335ee24776dab4a6a4e51c7cc5d3cdd8265105a7
Taekwan Park <techigh@Taekwans-MacBook-Pro.local>
2022-03-21 21:29:34 +0900

    roomUpdate

--- src/Message/roomUpdate/roomUpdate.resolvers.js
+++ src/Message/roomUpdate/roomUpdate.resolvers.js
@@ -2,39 +2,41 @@ import { withFilter } from "graphql-subscriptions";
 import client from "../../client";
 import { NEW_MESSAGE } from "../../constant";
 import pubsub from "../../pubsub";
-import { protectedResolver } from "../../user/user.utils";
+import { protectedResolver } from "../../User/user.utils";
 
 export default {
   Subscription: {
     roomUpdate: {
-      subscribe: async (root, args, { loggedInUser }, info) => {
-        const roomCheck = await client.room.findFirst({
-          where: {
-            id: args.roomId,
-            users: { some: { id: loggedInUser.id } },
-          },
-        });
-        if (!roomCheck) {
-          throw new Error("There is no room.");
-        }
-        return withFilter(
-          () => pubsub.asyncIterator(NEW_MESSAGE),
-          async (root, args, { loggedInUser }) => {
-            if (root.roomUpdate.roomId === args.roomId) {
-              const roomCheck = await client.room.findFirst({
-                where: {
-                  id: args.roomId,
-                  users: { some: { id: loggedInUser.id } },
-                },
-              });
-              if (!roomCheck) {
-                return false;
-              }
-              return true;
-            }
+      subscribe: protectedResolver(
+        async (root, args, { loggedInUser }, info) => {
+          const roomCheck = await client.room.findFirst({
+            where: {
+              id: args.roomId,
+              users: { some: { id: loggedInUser.id } },
+            },
+          });
+          if (!roomCheck) {
+            throw new Error("There is no room.");
           }
-        )(root, args, { loggedInUser }, info);
-      },
+          return withFilter(
+            () => pubsub.asyncIterator(NEW_MESSAGE),
+            async (root, args, { loggedInUser }) => {
+              if (root.roomUpdate.roomId === args.roomId) {
+                const roomCheck = await client.room.findFirst({
+                  where: {
+                    id: args.roomId,
+                    users: { some: { id: loggedInUser.id } },
+                  },
+                });
+                if (!roomCheck) {
+                  return false;
+                }
+                return true;
+              }
+            }
+          )(root, args, { loggedInUser }, info);
+        }
+      ),
     },
   },
 };
